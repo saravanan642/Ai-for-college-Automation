6cd2dd38293a3232bcced55c5529fdc09621de6a
saravanan642 <saravanansenthil605@gmail.com>
2026-03-04 15:30:57 +0530

    first commit

---
+++ src/Compoents/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+      <header>
+        <p className=' text-blue-900 text-5xl'>saravanan</p>
+      </header>
+    </div>
+  )
+}
+
+export default Header
